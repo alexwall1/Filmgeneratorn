9841678b11d2da919660fcc5991b4d2dfa4d67f0
Alexander Wall <wall.alexander@gmail.com>
2017-12-27 17:08:45 +0100

    Pre-rendered model for speed

--- app.py
+++ app.py
@@ -2,31 +2,24 @@
 
 from flask import Flask,render_template
 from flask import g
-import csv
 import markovify
 import random
+import json
 
 app=Flask(__name__)
 
-def get_corpus():
-	corpus=getattr(g,'_corpus',None)
-	if corpus is None:
-		with open('FilmReg85_95_utf8.csv','r') as csv_file:
-			reader=csv.reader(csv_file,delimiter=',')
-			corpus=[]
-			for row in reader:
-				if row[52]=='Spelfilm' and row[53]=='Långfilm' and row[56]:
-					corpus.append(row[56])
-		g._corpus=corpus
-	return corpus
+def load_model_json():
+	with open('model.json','r') as json_file:
+		model_json=json.load(json_file)
+		return model_json
 
 def generate_movie():
-	corpus=getattr(g,'_corpus',None)
-	if corpus is None:
-		corpus=get_corpus()
-	text_model=markovify.Text(corpus,retain_original=False)
-	plot=text_model.make_sentence_with_start("En film")
-	title=text_model.make_short_sentence(40)
+	model_json=getattr(g,'_model_json',None)
+	if model_json is None:
+		model_json=load_model_json()
+	model=markovify.Text.from_dict(model_json)
+	plot=model.make_sentence_with_start("En film")
+	title=model.make_short_sentence(40)
 	return plot,title
 
 @app.route('/')
@@ -37,4 +30,4 @@ def index():
 	year=random.randint(1985,1995)
 	genre=['Komedi','Skräck','Romantik','Erotik','Thriller','Dokumentär'][random.randint(0,5)].decode('utf-8')
 	rating="%.1f" % random.uniform(0,2)
-	return render_template('base.html',genre=genre,year=year,title=title.replace('.','').decode('utf-8'),rating=rating,plot=plot.decode('utf-8'))
+	return render_template('base.html',genre=genre,year=year,title=title.replace('.',''),rating=rating,plot=plot)
